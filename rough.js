fbf27885af2a3e4f4b7fc52212877002160a63cd
karan singh D <ceitkaransingh2026@gmail.com>
2024-02-02 18:41:48 +0530

    trash filr to move the file into the git repo

---
+++ rough.js
@@ -0,0 +1,46 @@
+function funcname(){
+  console.log("func executed");
+}
+funcname()
+funcname()
+funcname()
+
+const sum=(a,b)=>{
+  console.log(a+b);
+}
+
+sum(2,2)
+
+let arr=[2,3,3,4,55,89];
+
+arr.forEach(function printval(val){
+  console.log(val*2);
+
+})
+
+printval(arr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
